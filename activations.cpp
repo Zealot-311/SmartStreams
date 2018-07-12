bd634dd26671267a470eef6b2b275134744ab3e2
Jack Johnson <jack.johnson@ibm.com>
2018-07-12 17:59:49 -0500

    fixing vector thingy

--- activations.cpp
+++ activations.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <iostream>
 #include <map>
+#include "matrix.h"
 using namespace std;
 template <typename T>
 class fc
@@ -18,22 +19,22 @@ public:
 	int output_dim;
 	std::map<T,T> params;
 	std::map<T,T> grads;
-	std::vector<T> meta;
+	std::vector<std::vector<T> > meta;
 	void init(std::string name = "FC", int input_dim = 0, int output_dim = 0, float init_scale = 0){
 		name = name;
 		w_name = name + "_w";
 		b_name = name + "_b";
 		input_dim = input_dim;
 		output_dim = output_dim;
-		params.insert(std::pair<std::string, std::vector<T> >(w_name, util_scalar_mult(init_scale, util_random_matrix(input_dim, output_dim))));
-		params.insert(std::pair<std::string, std::vector<T> >(b_name, util_scalar_mult(init_scale, util_zero_matrix(output_dim))));
-		grads.insert(std::pair<std::string, std::vector<T> >(w_name, NULL));
-		grads.insert(std::pair<std::string, std::vector<T> >(b_name, NULL));
+		params.insert(std::pair<std::string, std::vector<std::vector<T> > >(w_name, util_scalar_mult(init_scale, util_random_matrix(input_dim, output_dim))));
+		params.insert(std::pair<std::string, std::vector<std::vector<T> >>(b_name, util_scalar_mult(init_scale, util_zero_matrix(output_dim))));
+		grads.insert(std::pair<std::string, std::vector<std::vector<T> > >(w_name, NULL));
+		grads.insert(std::pair<std::string, std::vector<std::vector<T> > >(b_name, NULL));
 		meta = NULL;
 	}
 
-	std::vector<T> forward(std::vector<T> feat){
-		std::vector<T> output = NULL;
+	std::vector<std::vector<T> > forward(std::vector<std::vector<T> > feat){
+		std::vector<std::vector<T> > output = NULL;
 		try{
 			if(input_dim != feat[0].size()){
 				throw(1);
@@ -46,8 +47,8 @@ public:
 		return output;
 	}
 
-	std::vector<T> backward(std::vector<T> dprev){
-		std::vector<T> feat = meta;
+	std::vector<std::vector<T> > backward(std::vector<std::vector<T> > dprev){
+		std::vector<std::vector<T> > feat = meta;
 		try {
 			if (feat == NULL){
 				throw 1;
@@ -56,12 +57,12 @@ public:
 		catch(int i){
 			cout << "No forward function called before for this module!";
 		};
-		std::vector<T> dfeat = NULL;
-		grads.insert(std::pair<std::string, std::vector<T> >(w_name, NULL));
-		grads.insert(std::pair<std::string, std::vector<T> >(b_name, NULL));
-		grads.insert(std::pair<std::string, std::vector<T> >(b_name, util_sum(dprev, 0)));
+		std::vector<std::vector<T> > dfeat = NULL;
+		grads.insert(std::pair<std::string, std::vector<std::vector<T> > >(w_name, NULL));
+		grads.insert(std::pair<std::string, std::vector<std::vector<T> > >(b_name, NULL));
+		grads.insert(std::pair<std::string, std::vector<std::vector<T> > >(b_name, util_sum(dprev, 0)));
 		dfeat = util_mult(dprev, util_reshape(util_transpose(params[w_name]), meta.size(), meta[0].size()));
-		grads.insert(std::pair<std::string, std::vector<T> >(w_name, util_mult(util_transpose(util_reshape(feat, 1, input_dim)),dprev)));
+		grads.insert(std::pair<std::string, std::vector<std::vector<T> > >(w_name, util_mult(util_transpose(util_reshape(feat, 1, input_dim)),dprev)));
 		meta = NULL;
 		return dfeat;
 	};
@@ -76,7 +77,7 @@ public:
 	std::string name;
 	std::map<T,T> params;
 	std::map<T,T> grads;
-	std::vector<T> meta;
+	std::vector<std::vector<T> > meta;
 
 	void init(std::string name = "relu"){
 		name = name;
@@ -85,15 +86,15 @@ public:
 		meta = NULL;
 		grads[name] = NULL;
 	};
-	std::vector<T> forward(std::vector<T> feat){
-		std::vector<T> output = NULL;
+	std::vector<std::vector<T> > forward(std::vector<std::vector<T> > feat){
+		std::vector<std::vector<T> > output = NULL;
 		output = util_max(feat, 0);
 		meta = feat;
 		return output;
 	};
 
-	std::vector<T> backward(std::vector<T> dprev){
-		std::vector<T> feat = meta;
+	std::vector<std::vector<T> > backward(std::vector<std::vector<T> > dprev){
+		std::vector<std::vector<T> > feat = meta;
 		try {
 			if (feat == NULL){
 				throw 1;
@@ -102,7 +103,7 @@ public:
 		catch(int i){
 			cout << "No forward function called before for this module!";
 		};
-		std::vector<T> dfeat = NULL;
+		std::vector<std::vector<T> > dfeat = NULL;
 		feat = meta;
 		//TODO: maybe take this
 		feat = util_div(util_max(feat,0), feat);
@@ -130,14 +131,14 @@ public:
 		meta = NULL;
 		grads[name] = NULL;
 	};
-	std::vector<T> forward(std::vector<T> feat){
-		std::vector<T> output = NULL;
+	std::vector<std::vector<T> > forward(std::vector<std::vector<T> > feat){
+		std::vector<std::vector<T> > output = NULL;
 		output = util_sigmoid(feat);
 		meta = feat;
 		return output.data;
 	};
-	std::vector<T> backward(std::vector<T> dprev){
-		std::vector<T> feat = meta;
+	std::vector<std::vector<T> > backward(std::vector<std::vector<T> > dprev){
+		std::vector<std::vector<T> > feat = meta;
 		try {
 			if (feat == NULL){
 				throw 1;
@@ -146,7 +147,7 @@ public:
 		catch(int i){
 			cout << "No forward function called before for this module!";
 		};
-		std::vector<T> dfeat = NULL;
+		std::vector<std::vector<T> > dfeat = NULL;
 		feat = meta;
 		feat = util_div(util_sigmoid(feat), feat);
 		dfeat = util_mult(dprev, feat);
@@ -154,3 +155,5 @@ public:
 		return dfeat.data;
 	};
 };
+
+
